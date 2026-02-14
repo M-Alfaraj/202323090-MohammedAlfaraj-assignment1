6acd73d744bf7b77c70709e30268fb154536b5e0
M-Alfaraj <s202323090@kfupm.edu.sa>
2026-02-14 20:43:13 +0300

    Initial commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,38 @@
+function toggleTheme(){
+    const body = document.body;
+    const buttonSymbol = document.getElementById("buttonTheme");
 
+    body.classList.toggle("darkMode");
+
+    if(body.classList.contains("darkMode")){
+        buttonSymbol.textContent = "☀️";
+    }
+    else{
+        buttonSymbol.textContent = "🌙";
+    }
+}
+
+function greetingMessage(){
+    //variables to store the current hour and message for the greetings
+    const currentHour = (new Date()).getHours();
+    let message = "";
+
+    //checks the current hour to determine the greeting
+    //
+    if(currentHour >= 5 && currentHour < 12){
+        message = "Good Morning!";
+    }
+    else if(currentHour >= 12 && currentHour < 18){
+        message = "Good Afternoon!";
+    }
+    else if(currentHour >= 18 && currentHour < 22){
+        message = "Good Evening!";
+    }
+    else{
+        message = "Greetings and good night!";
+    }
+
+    alert(message)
+}
+
+window.addEventListener("DOMContentLoaded",greetingMessage)
